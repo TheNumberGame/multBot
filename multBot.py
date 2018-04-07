6a2d7d1ad1a7373a144ce4c4060987be7f83bb6a
TheNumebrGame <skyler.chandos@yahoo.com>
2018-04-07 11:43:12 -0700

    can receive a subreddit

--- multBot.py
+++ multBot.py
@@ -5,6 +5,7 @@ import discord
 import praw
 
 discord_client = discord.Client()
+reddit = praw.Reddit("multBot")
 
 def load_discord_token():
 	with open("discord_credentials.txt") as fp:
@@ -15,12 +16,17 @@ def main():
 
 @discord_client.event
 async def on_message(message):
-	if message.content.startswith('//top'):
-		print(message.content)
-		await discord_client.send_message(message.channel, "message received")
+	if message.content.startswith('//hot'):
+		tokens = message.content.split(" ", 1)
+		
+		if len(tokens) == 1:
+			await discord_client.send_message(message.channel, "Give us a real subreddit")
+		else:
+			subreddit = reddit.subreddit(tokens[1])
+			submission = next(subreddit.hot())
+			await discord_client.send_message(message.channel, "https://www.reddit.com" + submission.permalink)
 	
 def main_reddit():
-	reddit = praw.Reddit("multBot")
 	subreddit = reddit.subreddit("multBot")
 	for submission in subreddit.stream.submissions():
 		if submission.saved:
