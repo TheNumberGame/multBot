9dc4bdbfd7af1d217d2f36e9d9b447e217106467
TheNumebrGame <skyler.chandos@yahoo.com>
2018-04-07 10:09:05 -0700

    Prevent duplicate processing

--- multBot.py
+++ multBot.py
@@ -5,8 +5,11 @@ def main():
 	reddit = praw.Reddit("multBot")
 	subreddit = reddit.subreddit("multBot")
 	for submission in subreddit.stream.submissions():
+		if submission.saved:
+			continue
 		print(submission.title)
 		submission.reply("A Comment")
+		submission.save()
 	
 if __name__ == "__main__":
 	main()
