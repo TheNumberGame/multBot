2d98d43683c8d36fbfd926e9ad0b8ae60729fd77
TheNumebrGame <skyler.chandos@yahoo.com>
2018-04-07 12:34:08 -0700

    "//top" command for multbot

--- multBot.py
+++ multBot.py
@@ -16,11 +16,13 @@ def main():
 
 @discord_client.event
 async def on_message(message):
-	await on_new(message)
-	await on_hot(message)
+	await on_submission(message, "hot")
+	await on_submission(message, "new")
+	await on_submission(message, "top")
 
-async def on_new(message):
-	if message.content.startswith('//new'):
+
+async def on_submission(message, category):
+	if message.content.startswith('//' + category):
 		tokens = message.content.split(" ", 1)
 		
 		if len(tokens) == 1:
@@ -28,34 +30,14 @@ async def on_new(message):
 		else:
 			try:
 				subreddit = reddit.subreddit(tokens[1])
-				submission = new_submission(subreddit)
+				submission = unstickied_submission(subreddit, category)
 				await discord_client.send_message(message.channel, "https://www.reddit.com" + submission.permalink)
 			except:
 				await discord_client.send_message(message.channel, "Invalid subreddit")
-
-
-async def on_hot(message):
-	if message.content.startswith('//hot'):
-		tokens = message.content.split(" ", 1)
-		
-		if len(tokens) == 1:
-			await discord_client.send_message(message.channel, "Give us a real subreddit")
-		else:
-			try:
-				subreddit = reddit.subreddit(tokens[1])
-				submission = hot_submission(subreddit)
-				await discord_client.send_message(message.channel, "https://www.reddit.com" + submission.permalink)
-			except:
-				await discord_client.send_message(message.channel, "Invalid subreddit")
-
-def hot_submission(subreddit):
-	for submission in subreddit.hot():
-		if not submission.stickied:
-			break
-	return submission
 	
-def new_submission(subreddit):
-	for submission in subreddit.new():
+
+def unstickied_submission(subreddit, listing):
+	for submission in getattr(subreddit, listing)():
 		if not submission.stickied:
 			break
 	return submission
