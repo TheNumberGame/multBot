054a9ca5211d46024f4be72935a7f5c928dd7ac0
TheNumebrGame <skyler.chandos@yahoo.com>
2018-04-07 15:45:26 -0700

    messaging between discord and reddit as if they were the same thing

--- multBot.py
+++ multBot.py
@@ -10,7 +10,9 @@ event_loop = asyncio.get_event_loop()
 discord_client = discord.Client(loop = event_loop)
 reddit = praw.Reddit("multBot")
 PINNED_SUBMISSION = reddit.submission(id="8alcez")
-mult_subreddit_stream = reddit.subreddit("multBot").stream.submissions(pause_after = -1)
+submission_stream = reddit.subreddit("multBot").stream.submissions(pause_after = -1)
+comment_stream = reddit.subreddit("multBot").stream.comments(pause_after = -1)
+DISCORD_USERNAME = "multBot"
 
 def load_discord_token():
 	with open("discord_credentials.txt") as fp:
@@ -21,15 +23,21 @@ def main():
 	discord_client.run(load_discord_token())
 
 async def reddit_task():
-	submission = next(mult_subreddit_stream)
+	submission = next(submission_stream)
 	while submission is not None:
-		if submission.saved:
-			submission = next(mult_subreddit_stream)
-			continue
-		await discord_client.send_message(discord_client.get_channel("432274642514739201"), "https://www.reddit.com" + submission.permalink)
+		if not submission.saved:
+			await discord_client.send_message(discord_client.get_channel("432274642514739201"), "https://www.reddit.com" + submission.permalink)
+			submission.save()
 		
-		submission.save()
-		submission = next(mult_subreddit_stream)
+		submission = next(submission_stream)
+	
+	comment = next(comment_stream)
+	while comment is not None:
+		if comment.submission == PINNED_SUBMISSION and comment.author != reddit.config.username and not comment.saved:
+			await discord_client.send_message(discord_client.get_channel("432295161301827595"), comment.author.name+": "+comment.body)
+			comment.save()
+		comment = next(comment_stream)
+	
 	event_loop.call_later(5, timed_callback)
 
 def timed_callback():
@@ -44,10 +52,10 @@ async def on_message(message):
 	await reddit_portal_comment(message)
 
 async def reddit_portal_comment(message):
-	if message.channel.id == "432295161301827595":
+	if message.channel.id == "432295161301827595" and message.author.name != DISCORD_USERNAME:
 		PINNED_SUBMISSION.reply(message.author.name + ": " + message.content)
-
 	
+		
 async def on_submission(message, category):
 	if message.content.startswith('//' + category):
 		tokens = message.content.split(" ", 1)
